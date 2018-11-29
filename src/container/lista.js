1c6ecb72452c56974e6db86f00c86fe76278852f
Cristian Torres <ctorresthrash@gmail.com>
2018-11-29 10:27:40 -0500

    fix container redux connection

--- src/container/lista.js
+++ src/container/lista.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import * as actions from '../actions/actions.js'
+import { connect } from 'react-redux';
+import actions from '../actions/actions.js'
 import Contenedor from '../component/index.js';
 
-class Lista extends React.Component{
+class Lista extends React.Component {
 
-    render(){
-        return(
+    render() {
+        return (
             <Contenedor
-            handleClick={this.props.loadArticles} 
-            articles={this.props.articles}></Contenedor>
+                handleClick={this.props.loadArticles}
+                articles={this.props.articles}></Contenedor>
         )
     }
 }
 
-const mapStateToProps = (state)=>{
-    return state
-};
-
-export default connect (mapStateToProps,actions)(Lista);
+export default connect(state => ({
+    articles: state.articles
+}), actions)(Lista);

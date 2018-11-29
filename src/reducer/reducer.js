cde93a0355a8e9e5e2288cefb79a399386f58af6
Cristian Torres <ctorresthrash@gmail.com>
2018-11-29 10:27:30 -0500

    fix reducer typo

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -3,11 +3,11 @@ let initialState={
 }
 
 const mainReducer=(state =initialState, action)=>{
-
+    console.log(action)
     switch(action.type){
         case "GET_ARTICLES":
         return { 
-            ...state, acticles: action.articles
+            ...state, articles: action.articles
             }
         default:
             return state

2a1a681724c1a939b0e9c4abdc5b1b8040d704c4
Cristian Torres <ctorresthrash@gmail.com>
2018-11-29 10:27:20 -0500

    fix actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-export function loadArticles(){
-    return (dispatch) =>{
-        return axios.get("https://myarticles-ivan.herokuapp.com/api/v1/articles").then((response)=>{
-            console.log(response);  
-            dispatch({type:"GET_ARTICLES", articles: response.data});
-        })
+export default {
+    loadArticles(){
+        return (dispatch) =>{
+            return axios.get("https://myarticles-ivan.herokuapp.com/api/v1/articles").then((response)=>{
+                dispatch({type:"GET_ARTICLES", articles: response.data});
+            })
+        }
     }
-}
+} 
